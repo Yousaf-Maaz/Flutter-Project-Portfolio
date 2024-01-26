235ddc511b3950f5df8b0b63dc9d8b28f19bb7e9
Yousaf-Maaz <yousafmaaz.kakakhel@gmail.com>
2024-01-27 01:33:34 +0500

    UI / profile pic /text color and design added

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,148 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: PortfolioScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class PortfolioScreen extends StatelessWidget {
+  const PortfolioScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: null,
+      body: Container(
+        color: Colors.black87, // Light Black background color
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/Yousaf Maaz.png'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Yousaf Maaz',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // Green text color
+                ),
+              ),
+              const SizedBox(height: 10),
+              RichText(
+                text: const TextSpan(
+                  text: 'LinkedIn: ',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                  children: [
+                    TextSpan(
+                      text: 'https://www.linkedin.com/in/yousaf-maaz/',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              RichText(
+                text: const TextSpan(
+                  text: 'GitHub: ',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 233, 236, 233),
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'https://github.com/YousafMaaz',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Divider(color: Color.fromARGB(255, 176, 189, 176)),
+              const ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.briefcase,
+                  color: Color.fromARGB(255, 249, 253, 249),
+                ),
+                title: Text(
+                  'Work Experience',
+
+                  style: TextStyle(color: Colors.white), // Adjusted text color
+                ),
+                subtitle: Text(
+                  'EXPERIENCE :'
+                  'C++ Development intern - InfraFlux, Pakistan  Remote    | '
+                  'Web Development Intern - CodSoft, Remote    | '
+                  'SynCrona, Peshawar - Frontend Development Trainee    | '
+                  'Zindigi Prize FAST NUCES Peshawar- Campus Director    | '
+                  'FAST Incubation Society  - Co-Head Event Management    | '
+                  'GDSC FAST Peshawar Campus - sponsors Lead    | '
+                  'IEEE FAST NUCES Pwr Student Branch - Junior Representative    ',
+                  style: TextStyle(color: Colors.white), // Adjusted text color
+                ),
+              ),
+              const Divider(color: Color.fromARGB(255, 236, 238, 236)),
+              const ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.graduationCap,
+                  color: Color.fromARGB(255, 233, 236, 233),
+                ),
+                title: Text(
+                  'Education',
+                  style: TextStyle(color: Colors.white), // Adjusted text color
+                ),
+                subtitle: Text(
+                  ' Fast National University(Fast-Nuces)  - Software Engineering'
+                  '- As a student here, I am not just focused on textbooks but also on applying what I learn in real-world scenarios. I believe in the'
+                  'power of connections and the value they bring to the learning process. ',
+                  style: TextStyle(color: Colors.white), // Adjusted text color
+                ),
+              ),
+              const Divider(color: Color.fromARGB(255, 222, 226, 222)),
+              const ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.code,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Skills',
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  'Flutter, Dart, C/C++, Html, css, javascript,Figma.',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
